4ffb737e0c0183db69bdb4040d0007e78d0bb90d
rick <ayso1sra@gmail.com>
2020-10-29 10:49:20 -0700

    2020-10-29:  - updated prepeAYSOcsv.sh to use py folder

--- data/prepeAYSOcsv.sh
+++ data/prepeAYSOcsv.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 cd ~/Google_Drive.rick.roberts.9/_ayso/s1/reports/data
 source ../scripts/prep_csv/venv/bin/activate
-python ../scripts/prep_csv/py/prep_csv.py
+python ../scripts/prep_csv/py/prep_eayso_csv.py
 deactivate
