07dac15190069171dff57c2e5f8fde691fd33f25
rick <ayso1sra@gmail.com>
2020-11-12 11:34:56 -0800

    2020-11-12:  - remove scripts folder from repository

--- scripts/htmlDataTables/pFC.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-cd ~/Google_Drive.rick.roberts.9/_ayso/s1/reports/scripts/htmlDataTables
-source ./venv/bin/activate
-rm -f ./html/*.csv
-python ./py/processFutureCourses.py ./html/Future_Courses_Report.html
-deactivate
--- scripts/mail_crawler/list_subjects.py
+++
@@ -1,6 +0,0 @@
-conn = imaplib.IMAP4_SSL("imap.gmail.com", 993)
-gmail_user = 'kiran.chandrashekhar'
-gmail_pwd = 'gmail_password_here'
-
-conn.login(gmail_user, gmail_pwd)
-conn.select()
