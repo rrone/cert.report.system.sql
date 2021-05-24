63a5f04f63460efda69949110ae0e4656317c2b7
rick <ayso1sra@gmail.com>
2021-05-24 11:23:30 -0700

    2021-05-21:  - Updated sort on referees_in_affinity_not_in_sports_connect.sql

--- sql/referees_in_affinity_not_in_sports_connect.sql
+++ sql/referees_in_affinity_not_in_sports_connect.sql
@@ -1,5 +1,17 @@
-SELECT 
-    DISTINCT MY, League1, Club1, ClubID1, IDNUM, AltID1, FirstName1, LastName1, DOB1, GenderCode1, email1, refGrade, refObtainDate 
+SELECT DISTINCT
+    MY,
+    League1,
+    Club1,
+    ClubID1,
+    IDNUM,
+    AltID1,
+    FirstName1,
+    LastName1,
+    DOB1,
+    GenderCode1,
+    email1,
+    refGrade,
+    refObtainDate
 FROM
     ayso1ref_services.AdminCredentialsStatusDynamic
 WHERE
@@ -7,4 +19,5 @@ WHERE
         AND AltID1 NOT IN (SELECT 
             AYSOID
         FROM
-            ayso1ref_services.crs_rpt_ref_certs);
+            ayso1ref_services.crs_rpt_ref_certs)
+ORDER BY League1, Club1, LastName1;
